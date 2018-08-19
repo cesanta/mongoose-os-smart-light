b49cb22e899c60c48f74983f2b811290b51f8ac0
Sergey Lyubka <sergey.lyubka@cesanta.com>
2018-08-19 18:29:32 +0100

    Continue on provisioning

--- backend/mobile-app/js/app.jsx
+++ backend/mobile-app/js/app.jsx
@@ -6,7 +6,7 @@ const { createHashHistory } = window.History;
 const Header = () => (
   <header className="jumbotron mt-3 py-2 mb-4">
     <h2>Mongoose OS Smart Light</h2>
-  </header >
+  </header>
 );
 
 const Device = ({ name }) => (
@@ -23,7 +23,7 @@ const Device = ({ name }) => (
 
 const DeviceList = ({ }) => (
   <div>
-    <h3 className="mb-3">My devices <a href="/provision" className="ml-3 btn btn-danger text-white float-right">Add device</a></h3>
+    <h3 className="mb-3">My devices <a href="/add1" className="ml-3 btn btn-danger text-white float-right">Add device</a></h3>
     <div className="list-group">
       <Device name="device_1" />
       <Device name="device_2" />
@@ -31,21 +31,83 @@ const DeviceList = ({ }) => (
   </div>
 );
 
-const Provision = ({ }) => (
-  <div>
-    <div><a href="/">&larr; back to device list</a></div>
-    <h4>Provision New Device</h4>
-    <div className="my-2 alert alert-info small">
-      In order to provision new device, go to your mobile phone settings
-      and join the WiFi network with prefix "MongooseOS".
-      After that, click on the "Add Device" button below,
-      and switch back to your home WiFi network.
-    </div>
-    <button>
-      Add New Device
-    </button>
-  </div>
-);
+class AddDeviceStep1 extends Component {
+  componentDidMount() {
+    this.getDeviceName();
+  }
+  getDeviceName() {
+    console.log('Getting device name ...');
+    fetch('http://192.168.4.1/rpc/Config.Get').then(res => this.setState({ res }));
+    if (!this.state.deviceName) setTimeout(() => this.getDeviceName(), 1000);
+  }
+  render({ }, { deviceName }) {
+    return (
+      <div>
+        <div><a href="/">&larr; back to device list</a></div>
+        <div className="my-3 alert alert-secondary text-muted small">
+          Go to your mobile phone settings
+          and join the WiFi network "MongooseOS-????".
+          Then enter WiFi name/pass and click on the button.
+        </div>
+        <div className="my-3 alert alert-warning text-muted small">
+          {JSON.stringify(this.state.res)}
+        </div>
+        <h4>Step 1. <small>Configure WiFi on a device</small></h4>
+        <input
+          type="text"
+          className="form-control mb-2 ssid"
+          placeholder="WiFi network name"
+        />
+        <input
+          type="password"
+          className="form-control mb-2 pass"
+          placeholder="WiFi password"
+        />
+        <a
+          className="btn btn-danger btn-block mb-1"
+          href="/add2"
+          disabled={!deviceName}
+          onClick={() => {
+            const email = document.querySelector('.ssid');
+            const password = document.querySelector('.pass');
+            const ev = email.value;
+            const pv = password.value;
+            email.value = '';
+            password.value = '';
+            dash.login(ev, pv).then(() => route('/'));
+          }}
+        >
+          {
+            deviceName ? 'Configure WiFi' :
+              <div>
+                <span className="spinner mr-2"></span>
+                Connecting to device ...
+              </div>
+          }
+
+        </a>
+      </div>
+    )
+  }
+};
+
+class AddDeviceStep2 extends Component {
+  componentDidMount() {
+  }
+  render({ }, { deviceName }) {
+    return (
+      <div>
+        <div><a href="/add1">&larr; back to step 1</a></div>
+        <div className="my-3 alert alert-secondary text-muted small">
+          Go to your mobile phone settings
+          and join the WiFi network "MongooseOS-????".
+          Then enter WiFi name/pass and click on the button.
+        </div>
+        <h4>Step 2. <small>Configure WiFi on a device</small></h4>
+      </div>
+    )
+  }
+};
 
 class App extends Component {
   componentDidMount() {
@@ -57,9 +119,10 @@ class App extends Component {
         <Header />
         <Router history={createHashHistory()} onChange={e => { this.currentUrl = e.url }}>
           <DeviceList path="/" default title="Device List" />
-          <Provision path="/provision" title="Add New Device" />
+          <AddDeviceStep1 path="/add1" title="Add 1" />
+          <AddDeviceStep2 path="/add2" title="Add 2" />
         </Router>
-      </div >
+      </div>
     )
   }
 };
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,8 +1,4 @@
 #include "mgos.h"
-#include "mgos_mqtt.h"
-#ifdef MGOS_HAVE_WIFI
-#include "mgos_wifi.h"
-#endif
 
 enum mgos_app_init_result mgos_app_init(void) {
   return MGOS_APP_INIT_SUCCESS;
